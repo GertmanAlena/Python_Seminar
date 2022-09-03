88e5b1ddb083241ef65b14df8732609944d80196
Alena <gertmanea@mail.ru>
2022-09-03 22:47:44 +0300

    .\1_seminar\

--- 1_seminar/exe_1.py
+++ 1_seminar/exe_1.py
@@ -1,3 +1,4 @@
+from random import randint
 # print(list(range(10))) 
 # print(list(range(10+1)))
 # print(list(range(2,4)))
@@ -28,14 +29,57 @@
 
 
 # найти второй максимум
-n = int(input())
-max1 = n
-max2 = -1
-while n != 0: #пока не введём 0
-    n = int(input())
-    if n > max1:
-        max2 = max1
-        max1 = n
-    elif n > max2:
-        max2 = n
-print(max2)
+# n = int(input())
+# max1 = n
+# max2 = -1
+# while n != 0: #пока не введём 0
+#     n = int(input())
+#     if n > max1:
+#         max2 = max1
+#         max1 = n
+#     elif n > max2:
+#         max2 = n
+# print(max2)
+
+# найти сумму минимума и максимума
+
+# num = [2, 8, -9, 7, 55, -1, 41]
+# max = num[0]
+# min = num[1]
+# for i in num:
+#     if i > max:
+#         max = i
+#     elif i < min:
+#         min = i
+# sum = min + max
+# print(sum)
+
+# coll = int(input('...'))
+# min = int(input('min -> '))
+# max = int(input('max -> '))
+
+# num = [randint(min, max) for i in range(coll)]
+# print(num)
+# max = num[0]
+# min = num[1]
+# for i in num:  
+#     if i > max:
+#         max = i
+#     elif i < min:
+#         min = i
+# sum = min + max
+# print(sum)
+
+
+
+x = float(input('x '))  #10
+y = float(input('y '))  #20
+
+n = (x * 15 / 100)     #1.5
+
+count = 0
+while x<y:
+    x = (x+n)
+    count += 1
+else:  
+    print(count)
