3fd872d6122f5e35368884958d9eef05b19a2660
Alena <gertmanea@mail.ru>
2022-09-06 21:08:29 +0300

    .\2_seminar\

---
+++ 2_seminar/HomeWork.py
@@ -0,0 +1,9 @@
+
+from ast import Del
+from os import remove
+from random import randint
+import string
+                                                # Задача 1
+
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
