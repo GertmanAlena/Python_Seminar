2173dedd0274bae56ee4f6f1f84d3c4d1083a232
Alena <gertmanea@mail.ru>
2022-09-07 20:15:58 +0300

    .\2_seminar\

--- 2_seminar/HomeWork_2.py
+++ 2_seminar/HomeWork_2.py
@@ -1,5 +1,6 @@
 
 from ast import Compare, Del
+import numbers
 from os import remove
 import os
 from random import randint
@@ -13,34 +14,34 @@ os.system('CLS')
 # 67.82 -> 23
 # 0.56 -> 11
 
-def inputFloat(prompt=None):  # ПРОВЕРКА НА INT
-    while True:
-        p = input(prompt)
-        try:
-            return float(p)
-        except ValueError:
-            print('Ошибка. Ожидалось вещественное число.')
-number = input('Введите дробное число: ')
-print('Вы ввели', number, end= ' ')
+# def inputFloat(prompt=None):  # ПРОВЕРКА НА INT
+#     while True:
+#         p = input(prompt)
+#         try:
+#             return int(p)
+#         except ValueError:
+#             print('Ошибка. Ожидалось вещественное число.')
+# number = inputFloat('Введите дробное число: ')
+# print('Вы ввели', number, end= ' ')
 
-separation = number.split('.')
-num1 = int(separation[0])
-num2 = int(separation[1])
+# separation = number.split('.')
+# num1 = int(separation[0])
+# num2 = int(separation[1])
     
-print(f'num1 [', num1, ']', 'num2 [', num2, ']')
+# print(f'num1 [', num1, ']', 'num2 [', num2, ']')
 
-def sum_num(num):
-    sum = 0
-    if int(num) < 0: num *= -1      
-    while int(num) > 0:
-        if num % 10 != 0:
-            a = num%10
-            sum += a
-            num = int(num/10)
-    else: sum = sum + num
-    return sum
+# def sum_num(num):
+#     sum = 0
+#     if int(num) < 0: num *= -1      
+#     while int(num) > 0:
+#         if num % 10 != 0:
+#             a = num%10
+#             sum += a
+#             num = int(num/10)
+#     else: sum = sum + num
+#     return sum
 
-print(sum_num(num1) + sum_num(num2))
+# print(sum_num(num1) + sum_num(num2))
 
 
                                                 # Задача 2
@@ -52,6 +53,25 @@ print(sum_num(num1) + sum_num(num2))
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+# def inputInt(prompt=None):  # ПРОВЕРКА НА INT
+#     while True:
+#         p = input(prompt)
+#         try:
+#             return int(p)
+#         except ValueError:
+#             print('Ошибка. Ожидалось вещественное число.')
+# number = inputInt('Введите число: ')
+# print('Вы ввели', number)
+
+# def Proizvedenie(number):
+#     i=1
+#     rez = 1
+#     while i <= number:
+#         rez *= i
+#         i+=1
+#         print(rez, end= ' ')
+#     return rez
+# Proizvedenie(number)
                                                 # Задача 3
 # Палиндромом называется слово, которое в обе стороны читается одинаково: "шалаш", "кабак".
 # А еще есть палиндром числа - смысл также в том, чтобы число в обе стороны читалось одинаково, 
@@ -59,7 +79,56 @@ print(sum_num(num1) + sum_num(num2))
 # Если перевернутое число не равно исходному, то они складываются и проверяются на палиндром еще раз.
 # Это происходит до тех пор, пока не будет найден палиндром.
 # Напишите такую программу, которая найдет палиндром введенного пользователем числа.  
-                                           
+
+def inputInt(prompt=None):  # ПРОВЕРКА НА INT
+    while True:
+        p = input(prompt)
+        try:
+            return int(p)
+        except ValueError:
+            print('Ошибка. Ожидалось вещественное число.')
+number = inputInt('Введите число: ')
+print('Вы ввели', number)
+
+def Polindrom(number):
+    pol_num1 = []
+    for i in number:
+        pol_num1.append(int(i))
+    return pol_num1
+    
+def PolindromTwo(pol_num1):
+    pol_num2 = pol_num1[len(pol_num1)-1::-1] #строка первая наоборот записана
+    return pol_num2
+
+def check_palindrome(pol_num1, pol_num2):
+    
+    # if pol_num1 == pol_num2:
+    #     print(f'число {pol_num1} полиндром')
+   
+    # elif len(pol_num1) == len(pol_num2):
+    #     c = [x+y for x, y in zip(pol_num1, pol_num2)]
+            
+    #     print(c)
+
+    if pol_num1 == pol_num2:
+        print(f'число {pol_num1} полиндром')
+   
+    else:
+        try:
+            if len(pol_num1) == len(pol_num2):
+                while pol_num1 != pol_num2: 
+                    c = [x+y for x, y in zip(pol_num1, pol_num2)]
+                    print(f'число {c} теперь полиндром')
+                    return c
+        except ValueError:
+            print('Ошибка.')
+
+
+pol_num1 = Polindrom(str(number))
+print('первое число ->', pol_num1)
+pol_num2 = PolindromTwo(pol_num1)
+print('второе число ->', pol_num2)
+check_palindrome(pol_num1, pol_num2)
                                                 # Задача 4
 # Реализуйте выдачу случайного числа
 # не использовать random.randint и вообще библиотеку random
