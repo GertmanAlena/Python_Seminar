75df17cac9b9c6ae442163caad402bc1ed508a57
Alena <gertmanea@mail.ru>
2022-09-09 23:54:11 +0300

    .\My.py

--- My.py
+++ My.py
@@ -1,14 +1,14 @@
-import os
-os.system('CLS')
+# import os
+# os.system('CLS')
 
-def random(_min:int, _max:int) -> int:
-    '''
-    Генерация случайного числа
-    params:
-    min - начало диапазона
-    max - конец диапазона
-    '''
-    d = _max - _min#9
-    ms = datetime.datetime.today().microsecond / (10 ** 6)
-    #print(f'{ms=}')
-    return _min + math.ceil(d * ms)
+# def random(_min:int, _max:int) -> int:
+#     '''
+#     Генерация случайного числа
+#     params:
+#     min - начало диапазона
+#     max - конец диапазона
+#     '''
+#     d = _max - _min#9
+#     ms = datetime.datetime.today().microsecond / (10 ** 6)
+#     #print(f'{ms=}')
+#     return _min + math.ceil(d * ms)
