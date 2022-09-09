5ba5936160de67f177e9eb615f86eca81937afd3
Alena <gertmanea@mail.ru>
2022-09-09 22:22:40 +0300

    my

--- My.py
+++ My.py
@@ -1,25 +1,14 @@
 import os
 os.system('CLS')
 
-# number = input(' введите число -> ')
-# count = 0
-# for i in number:
-#     count += 1
-# print(count)
-                                        # ***************************
-
-# print(int(input(' количество кубиков -> ')) // 8)
-                                        # ***************************
-# print(''.join(list(filter(lambda char: char.isdigit(), num))))
-
-
-def f(x):
-    if x == 1:
-        return 'Целое'
-    elif x == 2.3:
-        return 23
-    else:
-        return
-arg = 1
-print(f(arg))
-print(type(f(arg)))
+def random(_min:int, _max:int) -> int:
+    '''
+    Генерация случайного числа
+    params:
+    min - начало диапазона
+    max - конец диапазона
+    '''
+    d = _max - _min#9
+    ms = datetime.datetime.today().microsecond / (10 ** 6)
+    #print(f'{ms=}')
+    return _min + math.ceil(d * ms)
