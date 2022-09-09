ae30c440d42d8d4117ca65fbd4a8f60f610a9b39
Alena <gertmanea@mail.ru>
2022-09-09 20:40:26 +0300

    my

---
+++ My.py
@@ -0,0 +1,25 @@
+import os
+os.system('CLS')
+
+# number = input(' введите число -> ')
+# count = 0
+# for i in number:
+#     count += 1
+# print(count)
+                                        # ***************************
+
+# print(int(input(' количество кубиков -> ')) // 8)
+                                        # ***************************
+# print(''.join(list(filter(lambda char: char.isdigit(), num))))
+
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+arg = 1
+print(f(arg))
+print(type(f(arg)))
