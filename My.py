6cdb8967aa6c407fc7a2ab5a3c49d95cf5f60c29
Alena <gertmanea@mail.ru>
2022-09-11 12:32:05 +0300

    .\My.py

--- My.py
+++ My.py
@@ -1,26 +1,25 @@
+import datetime
+import math
 import os
 os.system('CLS')
 
-# def random(_min:int, _max:int) -> int:
-#     '''
-#     Генерация случайного числа
-#     params:
-#     min - начало диапазона
-#     max - конец диапазона
-#     '''
-#     d = _max - _min#9
-#     ms = datetime.datetime.today().microsecond / (10 ** 6)
-#     #print(f'{ms=}')
-#     return _min + math.ceil(d * ms)
+def random(_min:int, _max:int) -> int: 
+    ''' Генерация случайного числа params: min - начало диапазона max - конец диапазона '''
+    d = _max - _min
+    ms = datetime.datetime.today().microsecond / (10 ** 6) 
+    print(f'{ms=}') 
+    return _min + math.ceil(d * ms)
+
+print(random(1,10))
 
 # Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
 # ['gfh5', 'gfh2', '67', 'jy32', '3put'] - ищем 32 - находим по индексу 3
 
-word = ['gfh5', 'gfh2', '67', 'jy32', '3put']
-n = '32'
-count = 0
-for i in word:
-    if n in i:
-        print(i, 'индекс ', count)
-    count+=1
+# word = ['gfh5', 'gfh2', '67', 'jy32', '3put']
+# n = '32'
+# count = 0
+# for i in word:
+#     if n in i:
+#         print(i, 'индекс ', count)
+#     count+=1
     
