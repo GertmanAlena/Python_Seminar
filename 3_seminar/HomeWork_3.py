fd7923322b3066ce32218eef62f889a3319f4dc0
Alena <gertmanea@mail.ru>
2022-09-13 22:26:15 +0300

    .\3_seminar\

--- 3_seminar/HomeWork_3.py
+++ 3_seminar/HomeWork_3.py
@@ -10,7 +10,7 @@ os.system('CLS')
                     # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 # import random_numb
     
-# numbers = random_numb.Random_Number(-10,10,5)
+# numbers = random_numb.Random_Number(-10,10,7)
 # print(numbers)
     
 # def Summa_Numbers(numbers2):
@@ -25,7 +25,7 @@ os.system('CLS')
 
 # print(Summa_Numbers(numbers2))
 
-# '''второй вариант решения задачи'''
+'''второй вариант решения задачи'''
 # print(numbers[1::2])
 # print (sum(numbers[1::2]))
 
@@ -33,10 +33,10 @@ os.system('CLS')
         # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
         # Пример: [2, 3, 4, 5, 6] => [12, 15, 16];    [2, 3, 5, 6] => [12, 15]
 
- #import random_numb
- #First_List = random_numb.Random_Number()
- #'''импортировала из вайла рандомно создание списка'''
- #print('First_List ', First_List)
+# import random_numb
+# First_List = random_numb.Random_Number(1, 100, 10)
+
+# print('First_List ', First_List)
 
 # def Proizved_Alem(First_List): 
 #     '''функция умножения элементов'''
@@ -45,15 +45,15 @@ os.system('CLS')
 #     else:
 #         size = int((len(First_List)/2)+0.5)
         
- #    Res_List = []
- #    i=0
- #    while i <= size-1:
- #        Res_List.append(First_List[i]*First_List[(i+1)*-1])
- #        i+=1
- #    # print('Res_List   ', Res_List)
- #    return Res_List
+#     Res_List = []
+#     i=0
+#     while i <= size-1:
+#         Res_List.append(First_List[i]*First_List[(i+1)*-1])
+#         i+=1
+#     # print('Res_List   ', Res_List)
+#     return Res_List
 
- #print('Res_List   ', Proizved_Alem(First_List))
+# print('Res_List   ', Proizved_Alem(First_List))
 
             # 3-Задайте список из вещественных чисел. 
             # Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
@@ -62,20 +62,20 @@ os.system('CLS')
 # [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
 # [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
 
-#Spisok_Numbers = [4.07, 5.1, 8.2444, 6.98] 
-#print('Spisok_Numbers -> ', Spisok_Numbers)
-#Integer_Numbers = []
-#for i in Spisok_Numbers:
+# Spisok_Numbers = [1.1, 1.2, 3.1, 5.17, 10.02] 
+# print('Spisok_Numbers -> ', Spisok_Numbers)
+# Integer_Numbers = []
+# for i in Spisok_Numbers:
 #    '''
 #    берём часть после точки через split
 #    '''
 #    Integer_Numbers.append(str(str(i).split('.')[1]))
-#print('второй вариант ', Integer_Numbers)
+# print('второй вариант ', Integer_Numbers)
 
-#def Min_Max(Integer_Numbers):
+# def Min_Max(Integer_Numbers):
 #    '''
 #    в этой функции находим min и max, 
-#    если максимальное число <10 добавляем поль
+#    если максимальное число <10 добавляем ноль
 #    и вычисляем разницу
 #    '''
 #    raznica=0
@@ -88,7 +88,7 @@ os.system('CLS')
 #    print(raznica)
 #    return raznica
 
-#Min_Max(Integer_Numbers)
+# Min_Max(Integer_Numbers)
 
             # 4- Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
             # Подумайте, как это можно решить с помощью рекурсии.
@@ -99,27 +99,27 @@ os.system('CLS')
 # 2 -> 10
 
 # def Binary(num):
-    # '''без рекурсии'''
+#     '''без рекурсии'''
 #     bin = []
 #     while num > 0:
 #         bin.append(num%2)
 #         num//=2
 #     return bin
 
- #bin = Binary(int(input('-> '))) 
- #print(bin[::-1])   
+# bin = Binary(int(input('-> '))) 
+# print(bin[::-1])   
 
-bin = []
-def Binary(num, n):
-    '''рекурсия'''
-    if num == 0:
-        return 1
-    else:
-        bin.insert(n, num%2)
-        return Binary(num//2, n-1)
+# bin = []
+# def Binary(num, n):
+#     '''рекурсия'''
+#     if num == 0:
+#         return 1
+#     else:
+#         bin.insert(n, num%2)
+#         return Binary(num//2, n-1)
         
-num = Binary(int(input('-> ')), -1)
-print(bin)  
+# num = Binary(int(input('-> ')), -1)
+# print(bin)  
 
 
 
@@ -130,10 +130,10 @@ print(bin)
 # Решение оформлять в виде функций
 # По возможности добавляйте docstring
 
-#def Fibonacci_Pos(num):
-    # '''
-    # функция последовательности Фибоначчи с положительными числами
-    # '''
+# def Fibonacci_Pos(num):
+#     # '''
+#     # функция последовательности Фибоначчи с положительными числами
+#     # '''
 #    Fibonacci_Positive = []
 #    fib1 = 0
 #    fib2 = 1
@@ -146,10 +146,10 @@ print(bin)
 #            fib2 = fib1+fib2
 #            fib1 = time
 #    return Fibonacci_Positive
-#Fibonacci_Positive = Fibonacci_Pos(int(input('-> ')))
-#print(Fibonacci_Positive)
+# Fibonacci_Positive = Fibonacci_Pos(int(input('-> ')))
+# print(Fibonacci_Positive)
 
-#def Fibonacci_Neg(num):
+# def Fibonacci_Neg(num):
 #    '''
 #    функция последовательности Фибоначчи с отрицательными числами
 #    '''
@@ -165,8 +165,8 @@ print(bin)
 #            fib2 = fib1-fib2
 #            fib1 = time
 #    return Fibonacci_Negative
-#Fibonacci_Negative = Fibonacci_Neg(int(input('-> ')))
-#print(Fibonacci_Negative)
+# Fibonacci_Negative = Fibonacci_Neg(int(input('-> ')))
+# print(Fibonacci_Negative)
 
-#Oll_Fib = str(Fibonacci_Negative[::-1]+Fibonacci_Positive)
-#print(Oll_Fib)
+# Oll_Fib = str(Fibonacci_Negative[::-1]+Fibonacci_Positive)
+# print(Oll_Fib)
