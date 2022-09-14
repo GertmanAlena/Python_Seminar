a9739f4a02dc78786bf5c578b45a85527e7f28c9
Alena <gertmanea@mail.ru>
2022-09-14 20:19:56 +0300

    .\4_seminar\

--- 4_seminar/Students.py
+++ 4_seminar/Students.py
@@ -4,23 +4,35 @@ os.system('CLS')
 Students = {}
 Students = \
 	{
-		'Соловьёва Дарья': '5',
-		'Сорока Евшений': '4',
-		'Курочкина Мария': '4',
-		'Чижик Иван': '3',
-		'Уточкина Наталья': '5'
+		'Соловьёва Дарья': 1,
+		'Сорока Евшений': 4,
+		'Курочкина Мария': 4,
+		'Чижик Иван': 3,
+		'Уточкина Наталья': 5
 	}
-with open('students.txt', 'a', encoding='utf-8') as data:
-	data.writelines(Students)
+
+with open('students.txt', 'w', encoding='utf-8') as data:  # пока работа в txt через data
+	for i in Students:
+		data.write(i + '\n') #записали в txt файл через data.write наш словарь
 	
-print(Students)
+	Students['Василий'] = 5    #добавляем в словарь Василия
+	data.write('Василий\n')  #добавляем в txt Василия через data.write
+
+	print(Students)
+	
+with open('students.txt', 'a', encoding='utf-8') as data:
+	for k in Students:
+		'''
+		Students 'Соловьёва Дарья': 5  читается как
+		имя с индексом [к] имеет значение 5
+
+		'''
+		n = 5
+		if Students[k] == n:
+			data.write('\n'+k.upper())
+			x = k.upper()
+			print(x)
+		else:
+			data.write('\n'+k)
 
 
-# for k in Students.keys():
-# 	print(k)
-for item in Students:
-	for k in Students.values():
-		if k == 5:
-			print('{}:{}'.format(item, Students[k]))
-# for z in Students:
-# 	print(Students[z], end=' ')
