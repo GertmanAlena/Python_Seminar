5f1d9667bb005db50baa25b3043e5d5e19c5abc1
Alena <gertmanea@mail.ru>
2022-09-16 19:00:11 +0300

    .\4_seminar\

--- 4_seminar/HomeWork_4.py
+++ 4_seminar/HomeWork_4.py
@@ -35,21 +35,21 @@ os.system('CLS')
                                 # Не использовать множества.
                                 # [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
 
-#numbers = [1,1,1,1,2,2,2,3,3,3,4]
+numbers = [1,1,1,1,2,2,2,3,3,3,4]
 
-#def Sort(number):                
-#    Sort_Number = []
-#    num = number[0]
-#    Sort_Number.append(num)
-#    for i in number:
-#        if i == num:
-#            continue
-#        else:
-#            num = i
-#            Sort_Number.append(i)
+def Sort(number):                
+   Sort_Number = []
+   num = number[0]
+   Sort_Number.append(num)
+   for i in number:
+       if i == num:
+           continue
+       else:
+           num = i
+           Sort_Number.append(i)
 
-#    return Sort_Number
-#print(Sort(numbers))
+   return Sort_Number
+print(Sort(numbers))
 
                                 # 3 - В файле, содержащем фамилии студентов и их оценки, 
                                 # изменить на прописные буквы фамилии тех студентов,
@@ -141,50 +141,50 @@ os.system('CLS')
                     # файл второй:
                     # сжатый текст.
 
-with open('RLE.txt', 'w', encoding='utf-8') as data:
-    simbol = 'AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool'
-    for i in simbol:
-        data.writelines(i)
+# with open('RLE.txt', 'w', encoding='utf-8') as data:
+#     simbol = 'AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool'
+#     for i in simbol:
+#         data.writelines(i)
 
-with open('RLE.txt', 'r', encoding='utf-8') as data:
-    source_text = ''
-    for i in data:
-        source_text += i
+# with open('RLE.txt', 'r', encoding='utf-8') as data:
+#     source_text = ''
+#     for i in data:
+#         source_text += i
     
-    compressed_text = ''
-    # no_alements = ' '
-    alements = ''
-    i=0
-    while i < len(source_text):
+#     compressed_text = ''
+#     # no_alements = ' '
+#     alements = ''
+#     i=0
+#     while i < len(source_text):
         
-        alements = source_text[i]
-        count_alements = 1
-        while i + 1 < len(source_text) and source_text[i] == source_text[i+1]:
-                count_alements+=1
-                i+=1
-        compressed_text = compressed_text + (str(count_alements) + alements)
-        i+=1
-    print(compressed_text)
-    new = open('rez_RLE.txt', 'w',encoding='utf-8')
-    new.writelines(compressed_text)
-    new.close()
-with open('rez_RLE.txt', 'r', encoding='utf-8') as data:
-    text = ''
-    for i in data:
-        text+= i
-    print(text)
+#         alements = source_text[i]
+#         count_alements = 1
+#         while i + 1 < len(source_text) and source_text[i] == source_text[i+1]:
+#                 count_alements+=1
+#                 i+=1
+#         compressed_text = compressed_text + (str(count_alements) + alements)
+#         i+=1
+#     print(compressed_text)
+#     new = open('rez_RLE.txt', 'w',encoding='utf-8')
+#     new.writelines(compressed_text)
+#     new.close()
+# with open('rez_RLE.txt', 'r', encoding='utf-8') as data:
+#     text = ''
+#     for i in data:
+#         text+= i
+#     print(text)
    
-    decod = ''
-    alements = ''
-    vrem_int = ''
-    for i in text:
-        if i.isdigit():
-            vrem_int+=i
-        else: 
-            alements = i
-            decod += int(vrem_int)*alements
-            vrem_int = ''
-    print('decod -> ', decod)
+#     decod = ''
+#     alements = ''
+#     vrem_int = ''
+#     for i in text:
+#         if i.isdigit():
+#             vrem_int+=i
+#         else: 
+#             alements = i
+#             decod += int(vrem_int)*alements
+#             vrem_int = ''
+#     print('decod -> ', decod)
 
 
             
