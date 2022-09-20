53adc2283ca0cec42d92ace79b3c2ea531edd949
Alena <gertmanea@mail.ru>
2022-09-20 22:53:07 +0300

    .\My.py

--- My.py
+++ My.py
@@ -69,10 +69,10 @@ os.system('CLS')
 #     alement = numbers[j]
 #     count = 0
 #     for i in numbers:
-        
+
 #         if i == alement:
 #             count+=1
-    
+
 #     v_max = count
 #     if v_max > max:
 #         max = v_max
@@ -81,26 +81,127 @@ os.system('CLS')
 # print(numbers)
 # print(f'число {max_al} повторяется {max} раз(а)')
 
-numbers = [random.randrange(1, 5, 1) for i in range(10)]
-print(numbers)
-max = 0
-v_max = 0
-max_al = ''
-j = 0
-while j < len(numbers):
-    alement = numbers[0]
-    count = 0
-    i=numbers[0]
-    while i in numbers:
-        
-        if i == alement:
-            count+=1
-            numbers.remove(i)
-    
-    v_max = count
-    if v_max > max:
-        max = v_max
-        max_al = alement
-    j+=1
+# numbers = [random.randrange(1, 5, 1) for i in range(10)]
+# print(numbers)
+# max = 0
+# v_max = 0
+# max_al = ''
+# j = 0
+# while j < len(numbers):
+#     alement = numbers[0]
+#     count = 0
+#     i=numbers[0]
+#     while i in numbers:
 
-print(f'число {max_al} повторяется {max} раз(а)')
+#         if i == alement:
+#             count+=1
+#             numbers.remove(i)
+
+#     v_max = count
+#     if v_max > max:
+#         max = v_max
+#         max_al = alement
+#     j+=1
+
+# print(f'число {max_al} повторяется {max} раз(а)')
+
+
+def proverka(x):
+    if x<=3 and x>0:
+        return x
+    else:
+      x=int(input("введите число eще раз :"))
+      return proverka(x)
+
+
+ 
+
+def delenie(x,tottal):
+   
+    tottal= tottal-x      
+    print(tottal)
+    if tottal%x==0 and tottal==0:
+        return tottal
+    # if tottal == 1:
+    #     return tottal
+    #     print('победа')
+    else:
+        x = int(input('x = '))
+        return delenie(x,tottal)
+
+tottal = int(input('tottal = '))
+x = int(input('x = '))
+total2=(delenie(x,tottal))
+print(total2)
+
+
+
+#семинар 6
+import re
+ 
+# actions = {
+#   "^": lambda x, y: str(float(x)**float(y)),
+#   "*": lambda x, y: str(float(x) * float(y)),
+#   "/": lambda x, y: str(float(x) / float(y)),
+#   "+": lambda x, y: str(float(x) + float(y)),
+#   "-": lambda x, y: str(float(x) - float(y))
+# }
+ 
+# priority_reg_exp = r"\((.+?)\)"
+# action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+ 
+# def my_eval(expresion: str) -> str:
+ 
+#     while (match := re.search(priority_reg_exp, expresion)):
+#         expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+ 
+#     for symbol, action in actions.items():
+#         while (match := re.search(action_reg_exp.format(symbol), expresion)):
+#             expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+ 
+#     return expresion
+ 
+ 
+# exp = "(1 + 5) * (5 * (18 - 2)) / 5"
+# print(my_eval(exp), eval(exp))
+# expression = '-22+2'
+
+
+# def get_numbers(expression):
+#     numbers = []
+#     temp = ''
+#     expression += '='
+#     minus = -1 if expression[0]=='-' else 1
+#     expression = expression[1:] if expression[0]=='-' else expression
+#     for char in expression:
+#         if char.isdigit():
+#             temp += char
+#         else:
+#             numbers.append(temp)
+#             temp = ''
+#     numbers = list(filter(lambda char: char.isdigit(), numbers))
+#     numbers[0] = f'-{numbers[0]}'
+#     return numbers
+
+
+# def get_operators(expression):
+#     return list(filter(lambda char: char in '+-*/', expression))
+
+
+# def check_alpha(expression):
+#     return not any(filter(lambda char: char.isalpha(), expression))
+
+
+# def check_expression(numbers: list, opers: list):
+#     return True if len(numbers) > len(opers)  else False
+
+
+# if check_alpha(expression):
+#     numbers = get_numbers(expression)
+#     list_operators = get_operators(expression)
+#     if check_expression(numbers, list_operators):
+#         print(numbers, list_operators)
+#     else:
+#         print('Выражение неполное', numbers, list_operators)
+# else:
+#     print('Вы ввели буквы')
